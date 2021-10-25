e4b2afb97ac205d297e18914b55f1038c0463e65
biancacomanescu97 <biancacomanescu97@gmail.com>
2021-10-25 20:42:43 +0100

    Deleted unused script

--- script.js
+++
@@ -1,9 +0,0 @@
-$(window).on("click", ".small", function() {
-    me = this;
-    setTimeout( function() { $(me).addClass("big"); }, 1 );
-});
-
-$(window).on("click", ".big", function() {
-    me = this;
-    setTimeout( function() { $(me).removeClass("big"); }, 1 );
-});
